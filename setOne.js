77ac6d723ff08b47ff677b576282653f51e0f92a
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2023-03-29 14:48:51 -0600

    Initial commit

---
+++ setOne.js
@@ -0,0 +1,142 @@
+////////// PROBLEM 1 //////////
+
+// ***** Do not edit the code below *****
+function findGrape (arr) {
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[i])} is a grape`)
+        } 
+    }
+}
+// ***** Do not edit the code above *****
+
+/*
+The code above is an example of 'function declaration.' Please re-write the function in 'function expression' and 'arrow function' syntaxes. 
+*/
+
+// RE-WRITE THE ABOVE FUNCTION IN 'FUNCTION EXPRESSION' SYNTAX HERE.
+
+const findGrape2 = function (arr) {
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[i])} is a grape`)
+        } 
+    }
+}
+
+// RE-WRITE THE ABOVE FUNCTION IN 'ARROW FUNCTION' SYNTAX HERE.
+
+const findGrape3 = (arr) => {
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[i])} is a grape`)
+        } 
+    }
+}
+
+////////// PROBLEM 2 //////////
+/*
+Write a one line function (give a name of your choice) with an implicit return that when the function is invoked, it will print: 'The first parameter is [parameter1]. The second parameter is [parameter2]'.
+*/
+
+// CODE HERE
+const oneLiner = (para1,para2) => console.log(`The first parameter is ${para1}. The secound parameter is ${para2}.`)
+
+// INVOKE THE FUNCTION HERE. THE PARAMETERS TAKE ANY DATATYPE.
+
+oneLiner(`Nikita`,`yuna`)
+////////// PROBLEM 3 //////////
+/*
+Write a function called 'greeting' that takes 3 parameters: firstName, lastName, and a callback function. In the function, invoke the callback, passing in a concatenation of firstName and lastName. 
+Then, outside of the greeting function, invoke the greeting function, passing in the value firstName and lastName of your choice, and a callback function that takes a parameter of fullName. The callback function will print 'Hello, my full name is [fullName]'. 
+*/
+
+// CODE 'GREETING FUNCTION' HERE
+const greeting = (firstName, lastName,callback) =>{
+    let fullName = `${firstName} ${lastName}`
+    callback(fullName)
+}
+ const printName = (fullname) =>{
+    console.log(`Hello, my full name is ${fullname}`)
+ }
+
+
+// INVOKE 'GREETING FUNCTION' HERE
+
+greeting(`Tristan`,`Johnson`,printName)
+
+////////// PROBLEM 4 //////////
+
+// ***** Do not edit the code below *****
+let prices = [50, 33, 78, 100, 25]
+
+let totalCost = []
+// ***** Do not edit the code above *****
+
+/* 
+Write a function called 'pricesPlusTax' that takes 2 params: an array ('prices' array) and a callback function. Inside the function, loop over the 'prices' array. For every iteration, calculate a new total cost (original price plus 20% tax), then push it to the 'totalCost' array. After the loop, invoke the callback function, passing in the 'totalCost' array.
+*/
+
+// CODE HERE
+const pricesPlusTax = (arr,callback) =>{
+    for(i = 0; i < arr.length; i++){
+        let temp = (arr[i] * .20);
+        totalCost.push(temp.toFixed(2));
+      console.log(totalCost[i]);
+      
+    }
+    callback(totalCost);
+}
+const totalPrint = (totalC) => console.log(`The new array plus tax = ${totalC}`)
+/* 
+Invoke the 'pricesPlusTax' function, passing in the 'prices' array and a callback function (passing in 'totalCost' as a param) that will print "The new array plus tax = [totalCost]"
+*/
+
+// CODE HERE
+pricesPlusTax(prices,totalPrint)
+
+////////// PROBLEM 5 //////////
+
+/* 
+A function can return another function. Let's create one!
+
+Create a function called 'multiplyingFactory' that takes a number as a param. The function returns another function that takes another number as a param. 
+
+The inner function should run this logic: if the first number passing in is greater than and equal to 5, print the multiplication of the first and second numbers. If not, print "Cannot multiply: the first number is smaller than 5." 
+*/
+
+// CODE HERE
+
+   
+
+const multiplyingFactory = (num) => (num2) => {
+    if(num >= 5){
+        console.log(num2 * num)
+    } else {
+        console.log("Cannot multiply: the first number is smaller than 5.")
+    }
+}
+   
+
+
+/* 
+Let's invoke the 'multiplyingFactory' function that will return another function, and save it into a variable called 'timesFour.' Let's pass in number 3 as a param.
+*/
+
+// CODE HERE
+const timesFour = multiplyingFactory(5);
+
+/* 
+Now, timesFour is the new function (the inner function that was being returned when we invoked 'multiplyingFactory' function). The number 3 that we passed in as a first number is now saved in the 'timesFour' function. 
+
+Let's invoke 'timesFour' and pass in number 4 as a param. Number 4 here is the second number that will multiply the first number (number 3).
+
+Run the code in node to see the printed result. You should see "Cannot multiply: the first number is smaller than 5."
+*/
+
+// INVOKE 'timesFour' HERE
+timesFour(4)
+
+/* 
+Change the param for 'multiplyingFactory' invocation to number 5. Then invoke 'timesFour' again, passing in number 4. Run the code in node, and you should see 20.
+*/
---
+++ setTwo.js
@@ -0,0 +1,289 @@
+////////// PROBLEM 1 //////////
+
+/*
+  Write a function called multiply that takes in three parameters: two numbers and a callback function.  
+  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
+*/
+
+// CODE HERE
+const multiply = (num1,num2,callback) => {
+  callback(num1 * num2)
+}
+
+// UNCOMMENT THE FUNCTION CALL BELOW
+// RUN THIS FILE WITH NODE
+// CHECK YOUR ANSWER
+
+multiply(4, 3, answer => {
+  console.log('The answer is ' + answer) //should console.log 12
+})
+
+
+
+////////// PROBLEMS 2 - 6 //////////
+
+// The names array will be used in problems 2 - 6.
+
+// Do not edit the code below.
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan']
+// Do not edit the code above.
+
+
+
+////////// PROBLEM 2 //////////
+
+/*
+  Write a function called first that takes in two parameters, an array and a callback function.
+  Then invoke the callback function, passing in the first element in the array as it's argument.
+*/
+
+// CODE HERE 
+const first = (arr,callback) =>{
+  callback(arr[0])
+}
+
+// UNCOMMENT THE FUNCTION CALL BELOW
+// RUN THIS FILE WITH NODE
+// CHECK YOUR ANSWER
+
+first(names, firstName => {
+  console.log('The first name in names is ' + firstName)
+})
+
+
+
+////////// PROBLEM 3 //////////
+
+/*
+  Write a function called last that takes in an array and a callback function. 
+  Then invoke the callback, passing in the last element in the array as the argument.
+*/
+
+// CODE HERE
+const last = (arr,callback) =>{
+  callback(arr[arr.length - 1])
+}
+
+
+// UNCOMMENT THE FUNCTION CALL BELOW
+// RUN THIS FILE WITH NODE
+// CHECK YOUR ANSWER
+
+last(names, lastName => {
+  console.log('The last name in names is ' + lastName)
+})
+
+
+
+////////// PROBLEM 4 //////////
+
+/*
+  Write a function called contains that takes in three parameters: an array, a name and a callback.  
+  Check if the name exists in the array. 
+  If it does, invoke the callback with true as the argument. 
+  If the name does not exist, invoke the callback with false as the argument.
+*/
+
+// CODE HERE 
+const contains = (arr,name,callback) =>{
+  
+  for(i = 0; i < arr.length; i++)
+  {
+    if(arr[i] === name){
+      return callback(true)
+    }
+  }
+  callback(false)
+}
+
+// UNCOMMENT THE FUNCTION CALL BELOW
+// RUN THIS FILE WITH NODE
+// CHECK YOUR ANSWER
+
+contains(names, 'Colt', result => {
+  if(result === true){
+    console.log('Colt is in the array')
+  } else {
+    console.log('Colt is not in the array')
+  }
+})
+
+
+
+////////// PROBLEM 5 //////////
+
+/*
+  Write a function called uniq that takes in an array and a callback function.
+  Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
+  Hint: you can use a nested for loop to do this.
+*/
+
+// CODE HERE
+const uniq = (arr,callback) =>{
+  for(i = 0; i < arr.length; i++){
+    for(let j = i + 1;j < arr.length;j++){
+      //
+      if(arr[i] == arr[j]){      
+        arr.splice(j,1)
+        console.log(i,j)
+        j--;
+      }
+    }
+  }
+  callback(arr)
+}
+/*
+  Invoke the uniq function, passing in the names array from above and a callback function.
+  The callback function should take in one parameter called uniqArr.
+  The callback should print a string that says:
+  'The new names array with all the duplicate items removed is [UNIQARRPARAM].'
+*/
+
+// CODE HERE
+
+uniq(names, result =>{
+  console.log(`The new names array with all the duplicate items removed is ${result}.`)
+})
+
+////////// PROBLEM 6 //////////
+
+/* 
+  Write a function called each that takes in an array of names and a callback function. 
+  For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
+*/
+
+// CODE HERE 
+
+const each = (arr,callback) =>{
+  for(i = 0; i < arr.length; i++){
+    callback(arr[i], i)
+  }
+}
+/*
+  Invoke the each function, passing in the names array and a callback function.
+  The callback function should take in two parameters, item and index.
+  The callback should print a string that says:
+  'The item at index [INDEXPARAM] is [ITEMPARAM].'
+*/
+
+// CODE HERE
+const index = (arr,index) =>{
+  console.log(`The item at index ${index} is ${arr}.`)
+}
+
+each(names,index)
+
+
+////////// CHALLENGES //////////
+
+
+////////// CHALLENGE 1 //////////
+
+
+/*
+  You'll be writing a higher order function that returns
+  another function. 
+
+  Create a function called addingFactory that takes in
+  one parameter (it will be a number).
+
+  addingFactory should return a function that takes in
+  one parameter (this will be another number).
+
+  The (inner) function that's being returned should add
+  the two parameters together and return the sum.
+*/
+
+// CODE HERE
+const addingFactory = (num1) => (num2) => num1 + num2;
+/*
+  Now that you have addingFactory, you can create other
+  functions from it. 
+
+  You're going to invoke addingFactory and save the result
+  to a new variable. 
+
+  Create a variable called addTen and set it equal to 
+  the invocation of addingFactory passing in the number
+  10 as an arguemnt.
+*/
+
+// CODE HERE
+const addTen = addingFactory(10);
+/*
+  Now the inner function is stored in the addTen variable! 
+
+  Call the addTen function passing in another number and
+  console log the result.
+
+  Call it a second time, passing in a different number
+  to see the different outputs.
+*/
+
+// CODE HERE
+console.log(addTen(2));
+/*
+  Let's make another function from the addingFactory. 
+
+  This time, pass in your favorite number and name
+  the variable addNUMBER with the name of the number
+  you chose. 
+
+  Once you create that, you can invoke the function
+  to add any number to your favorite number!
+*/
+
+// CODE HERE
+const addNUMBER = addingFactory(5)
+
+console.log(addNUMBER(50));
+
+////////// CHALLENGE 2 //////////
+
+/*
+  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+  When the correct user object is found, invoke the callback with the user object as an argument.
+  You'll learn about objects in our next lecture. But you can research objects and attempt this problem.
+*/
+
+// Do not edit the code below.
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+]
+// Do not edit the code above.
+console.log(users[0].id)
+// CODE HERE 
+
+const getUserById = (arr, id, callback) =>{
+  for(i = 0; i < arr.length; i++){
+   
+    if(arr[i].id === id){
+      return callback(arr[i]);
+    }
+  }
+}
+
+// UNCOMMENT THE FUNCTION CALL BELOW
+// RUN THIS FILE WITH NODE
+// CHECK YOUR ANSWER
+
+getUserById(users, '16t', user => {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
+})
